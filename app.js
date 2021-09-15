7c7806f40225f0054e33be71fa86b66f918e06b9
Noah <noahmhodgson@gmail.com>
2021-09-15 12:21:55 -0400

    Homepage updated with placeholders

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+//globals
+var username;
+
 function loginBlock(){
     alert("Please Login to Access the Site")
 }
 
+function updateUserName(){
+    username = document.getElementById("name").value;
+    sessionStorage.setItem("name",username)
+}
 
-
+function showUsername(){
+    username = sessionStorage.getItem("name")
+    document.getElementById("nav").innerHTML += "<span class='navbar-text' style='text-align:center'>Current User: " + username +"</span>"
+}
