076e7337cc111b26508d8783d516a81ad5e680a3
Pang17 <36525054+Pang17@users.noreply.github.com>
2021-09-23 10:16:25 -0400

    Added news api functionality

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 //globals
 var username;
 
-function loginBlock(){
+function loginBlock() {
     alert("Please Login to Access the Site")
 }
 
-function updateUserName(){
+function updateUserName() {
     username = document.getElementById("name").value;
-    sessionStorage.setItem("name",username)
+    sessionStorage.setItem("name", username)
 }
 
-function showUsername(){
+function showUsername() {
     username = sessionStorage.getItem("name")
-    document.getElementById("nav").innerHTML += "<span class='navbar-text' style='text-align:center'>Current User: " + username +"</span>"
+    document.getElementById("nav").innerHTML += "<span class='navbar-text' style='text-align:center'>Current User: " + username + "</span>"
+}
+
+function getNews() {
+    
+    let input = document.getElementById("searchInput").value
+    const newsList = document.querySelector('.news-list');
+    newsList.innerHTML = ''
+    console.log(input)
+
+    const apiKey = '94381b289d5b494eae3bea618848ad38'
+
+    let url = `https://newsapi.org/v2/everything?q=${input}&apiKey=${apiKey}`
+    
+    console.log(url);
+
+    fetch(url).then((res) => {
+        return res.json()
+    }).then((data) => {
+        console.log(data);
+
+        data.articles.forEach(article => {
+            let li = document.createElement('li');
+            let a = document.createElement('a');
+
+            let img = document.createElement('img')
+            let imgURL = article.urlToImage;
+            img.src = imgURL;
+            img.width = 300;
+            img.height = 200;
+            a.setAttribute('href', article.url);
+            a.setAttribute('target', '_blank');
+            a.textContent = article.title;
+            img.setAttribute('image', article.urlToImage);
+
+            li.appendChild(a);
+            li.appendChild(img);
+            newsList.appendChild(li);
+        })
+    })
 }
