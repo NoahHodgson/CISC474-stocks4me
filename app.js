a913b61808480e7abc0f4ba0845ad72c9a1c2a2d
Justin Hamilton <jwhamilton99@gmail.com>
2021-09-26 01:51:35 +0000

    fix price display, 403 error, and symbol not found

--- app.js
+++ app.js
@@ -54,14 +54,18 @@ function search() {
 
     let r = new XMLHttpRequest();
     r.open("GET", "https://finnhub.io/api/v1/search?q=" + term + "&token=c548e3iad3ifdcrdgh80", true);
-    r.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+    r.onload = function () {
+        if (this.status == 200) {
             document.getElementById("stock-search-results").style["visibility"] = "visible";
             let obj = JSON.parse(this.response);
-            let firstResult = obj["result"][0];
-            getPriceForObject(firstResult);
-            getHistoryForObject(firstResult);
-            initWebSocket(firstResult);
+            if(parseInt(obj["count"]) > 0) {
+                let firstResult = obj["result"][0];
+                getPriceForObject(firstResult);
+                getHistoryForObject(firstResult);
+                initWebSocket(firstResult);
+            } else {
+                document.getElementById("name").innerHTML = "Could not find symbol."
+            }
         }
     }
     r.send();
@@ -87,7 +91,7 @@ function initWebSocket(object) {
 
             let newDiff = Math.round((currentDiff - (currentPrice - lastPrice)) * 100) / 100;
 
-            document.getElementById("price").innerHTML = "Current: " + lastPrice + " (" + ((newDiff >= 0) ? "+" : "") + newDiff + ") at " + date.getHours() + ":" + date.getMinutes();
+            document.getElementById("price").innerHTML = "Current: " + lastPrice + " (" + ((newDiff >= 0) ? "+" : "") + newDiff + ") at " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
         }
     };
     socket.onopen = function (e) {
@@ -109,12 +113,16 @@ function getPriceForObject(object) {
     let r = new XMLHttpRequest();
     r.open("GET", "https://finnhub.io/api/v1/quote?symbol=" + symbol + "&token=c548e3iad3ifdcrdgh80", true);
     r.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let obj = JSON.parse(this.response);
-            currentPrice = parseInt(obj["c"]);
-            currentDiff = parseInt(obj["d"]);
-            console.log(obj);
-            displayValue(object, obj);
+        if (this.readyState == 4) {
+            if(this.status == 200) {
+                let obj = JSON.parse(this.response);
+                currentPrice = parseFloat(obj["c"]);
+                currentDiff = parseFloat(obj["d"]);
+                console.log(obj);
+                displayValue(object, obj);
+            } else if(this.status == 403) {
+               document.getElementById("name").innerHTML = "Cannot access this symbol." 
+            }
         }
     }
     r.send();
@@ -153,7 +161,7 @@ function displayValue(infoObject, priceObject) {
     let d = new Date(priceObject['t']);
 
     document.getElementById("name").innerHTML = infoObject["description"] + " (" + infoObject["symbol"] + ")";
-    document.getElementById("price").innerHTML = "Current: " + lastPrice + " (" + ((newDiff >= 0) ? "+" : "") + newDiff + ") at " + date.getHours() + ":" + date.getMinutes();
+    document.getElementById("price").innerHTML = "Current: " + currentPrice + " (" + ((currentDiff < 0) ? "" : "+") + currentDiff + ") at " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     document.getElementById("highLow").innerHTML = "High/Low: " + priceObject["h"] + " / " + priceObject["l"];
 }
 
