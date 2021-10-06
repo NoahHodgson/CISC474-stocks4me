392db932c1d24c06c557c1e04b9552957cd0f41e
Noah <noahmhodgson@gmail.com>
2021-10-05 20:31:23 -0400

    removed infinite charts

--- chart.js
+++ chart.js
@@ -1,7 +1,9 @@
 function loadChart(data) {
   console.log("LOADING CHART");
   var margin = 50;
-  
+  //clear chart
+  chart = document.getElementById("stockChartContainer")
+  chart.innerHTML = ''
   // append the svg object to the body of the page
   var container = d3.select("#stockChartContainer");
   console.log(container.node());
@@ -16,45 +18,6 @@ function loadChart(data) {
   
   var graphContainer = svg.append("g")
     .attr("transform","scale("+((width-margin*2)/width)+") translate("+margin+",0)")
-  
-  /*
-  //load data
-  d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
-  
-    // read/parse the csv file
-    function(d){
-      return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-    },
-  
-    // dataset:
-    function(data) {
-  
-      // x axis 
-      var x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, width ]);
-      svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-  
-      // y axis
-      var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return +d.value; })])
-        .range([ height, 0 ]);
-      svg.append("g")
-        .call(d3.axisLeft(y));
-  
-      // line
-      svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "blue")
-        .attr("stroke-width", 1.75)
-        .attr("d", d3.line()
-          .x(function(d) { return x(d.date) })
-          .y(function(d) { return y(d.value) })
-          )
-  })*/
   
   console.log(d3.extent(data, function(d) {return d.date;}));
   
