5a9d41e9c23a7315071de2cbb3b3e593fbf8c660
Noah <noahmhodgson@gmail.com>
2021-11-04 13:06:32 -0400

    added a top story of the day

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //globals
 var username;
 var userwallet;
-var userstocks=[];
-var usernews=[];
+var userstocks = [];
+var usernews = [];
 var stockLoaded;
 var newsLoaded;
 var priceLoaded;
@@ -32,7 +32,7 @@ function showUsername() {
 function showUsernameSpecial() {
     username = sessionStorage.getItem("name")
     document.getElementById("nav-user").innerHTML += "<a class='nav-link active' style='padding:10px;' href='user.html'>User: " + username + "<br>Wallet: $" + userwallet.toFixed(2) + "</a>"
-    document.getElementById("user-username").innerHTML=username;
+    document.getElementById("user-username").innerHTML = username;
 }
 
 //initializes the user wallet at zero. CALL ONLY ONCE.
@@ -46,8 +46,8 @@ function getUserWallet() {
     userwallet = parseFloat(sessionStorage.getItem("wallet"))
 }
 
-function displayUserPageWallet(){
-    document.getElementById("user-userwallet").innerHTML="$"+userwallet;
+function displayUserPageWallet() {
+    document.getElementById("user-userwallet").innerHTML = "$" + userwallet;
 }
 
 //call when changing the wallet by adding funds or buying stocks
@@ -171,6 +171,48 @@ function columnFlipper(val) {
     }
 }
 
+async function getHomeNews() {
+    var homeNewsDiv = document.getElementById("stockChartContainer");
+    var topStoryURL = 'https://api.nytimes.com/svc/topstories/v2/business.json?api-key=0lRut9I2IboC0FlDg5wVabXmfIfb2hRU'
+    function processData(url) {
+        return new Promise((resolve, reject) => {
+            console.log(url);
+            const options = {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json"
+                },
+            };
+            fetch(url, options).then((res) => {
+                //console.log(res.text())
+                return res.text()
+            }).then((data) => {
+                data = JSON.parse(data);
+                resolve(data)
+            })
+        })
+    }
+    var topStoryObj = await processData(topStoryURL)
+    console.log(topStoryObj.results[0]);
+    let div = document.createElement('div')
+    let a = document.createElement('a');
+
+    let img = document.createElement('img')
+    let imgURL = topStoryObj.results[0].multimedia[0].url;
+    img.src = imgURL;
+    img.width = 600;
+    img.height = 300;
+    div.innerHTML = `<div style="margin: left; text-allign:center; padding-left: 20%">
+                    <h2>Today's Top Story:</h2>
+                    <a href=${topStoryObj.results[0].url} target="_blank">
+                        <img src=${img.src}
+                        width=${img.width} height=${img.height} style="display: blocked">
+                    </a>
+                    <div style="margin-top:1%;  width: 80%; text-allign:"><h4>${topStoryObj.results[0].title}</h4></div>
+                </div>`
+    homeNewsDiv.appendChild(div);
+}
+
 function getNews() {
     var table = document.getElementById('table_holder')
     table.innerHTML = `<div class="row justify-content-center" style="padding: 10px;" id="table_holder">
@@ -187,43 +229,43 @@ function getNews() {
     var column_flip = "news-c1" // flip this back and forth to fill collumns
     uniq_stocks = Array.from(new Set(usernews))
     console.log(uniq_stocks)
-    var first_symb=[]
+    var first_symb = []
     uniq_stocks.forEach(
-        s=>{
-            flag=1;
+        s => {
+            flag = 1;
             var array = s.split(" ")
-            array.forEach(o=>{
-                filter="LTD CO CORP INC INC-CLASS"
-                if(filter.includes(o)){ 
+            array.forEach(o => {
+                filter = "LTD CO CORP INC INC-CLASS"
+                if (filter.includes(o)) {
                     flag = 0;
                 }
             })
-            if(flag){
+            if (flag) {
                 first_symb.push(s);
             }
-            else{
+            else {
                 first_symb.push(array[0]);
             }
-    })
+        })
     //awful code
     uniq_stocks = first_symb;
     console.log(uniq_stocks)
     //date setup 
     var currentDate = new Date();
-    var pastDateNum = currentDate.getDate()-30;
+    var pastDateNum = currentDate.getDate() - 30;
     var pastDate = new Date();
     pastDate.setDate(pastDateNum)
-    function add_zero(str){
-        if(str.length==1){
-            return "0"+str;
+    function add_zero(str) {
+        if (str.length == 1) {
+            return "0" + str;
         }
-        else{
+        else {
             return str;
         }
     }
     current_string = currentDate.getFullYear().toString() + add_zero(currentDate.getMonth().toString()) + add_zero(currentDate.getDay().toString())
     past_string = pastDate.getFullYear().toString() + add_zero(pastDate.getMonth().toString()) + add_zero(pastDate.getDay().toString())
-    console.log(past_string +" " + current_string)
+    console.log(past_string + " " + current_string)
     //trying Promises - https://codeburst.io/javascript-making-asynchronous-calls-inside-a-loop-and-pause-block-loop-execution-1cb713fbdf2d
     function processData(url) {
         return new Promise((resolve, reject) => {
@@ -231,7 +273,7 @@ function getNews() {
             const options = {
                 method: "GET",
                 headers: {
-                  "Accept": "application/json"
+                    "Accept": "application/json"
                 },
             };
             fetch(url, options).then((res) => {
@@ -245,7 +287,7 @@ function getNews() {
     }
     //loading stories
     var all_articles = []
-        uniq_stocks.forEach(
+    uniq_stocks.forEach(
         (stock) => {
             const apiKey = '0lRut9I2IboC0FlDg5wVabXmfIfb2hRU'
             const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${past_string}&end_date=${current_string}&facet=false&q=${stock}&sort=relevance&api-key=${apiKey}`;
@@ -253,14 +295,14 @@ function getNews() {
         })
     Promise.all(all_articles).then((full_article_list) => {
         console.log(full_article_list)
-        cleaned_list=full_article_list
+        cleaned_list = full_article_list
         cleaned_list = cleaned_list.flat()
         console.log(cleaned_list)
         //shuffling list -
-        function fy(a,b,c,d){//array,placeholder,placeholder,placeholder
-            c=a.length;while(c)b=Math.random()*(--c+1)|0,d=a[c],a[c]=a[b],a[b]=d
+        function fy(a, b, c, d) {//array,placeholder,placeholder,placeholder
+            c = a.length; while (c) b = Math.random() * (--c + 1) | 0, d = a[c], a[c] = a[b], a[b] = d
         }
-        fy(cleaned_list,[],[])
+        fy(cleaned_list, [], [])
         //creating pages - https://stackoverflow.com/questions/55331172/pass-array-to-includes-javascript
         cleaned_list.forEach(article => {
             if (article.multimedia.length === 0) {
@@ -272,7 +314,7 @@ function getNews() {
                 let a = document.createElement('a');
 
                 let img = document.createElement('img')
-                let imgURL="https://www.nytimes.com/"+article.multimedia[0].url;
+                let imgURL = "https://www.nytimes.com/" + article.multimedia[0].url;
                 img.src = imgURL;
                 img.width = 300;
                 img.height = 200;
@@ -294,10 +336,10 @@ function getNews() {
 //Stock Search Info
 function search() {
     let id = "stockChartContainer";
-    document.getElementById(id).innerHTML="";
+    document.getElementById(id).innerHTML = "";
     let infoID = "stockInfo";
     let term = document.getElementById("searchInput").value.toUpperCase();
-    document.getElementById("stock-search-results").innerHTML=`<div class="module" id="stockInfo">
+    document.getElementById("stock-search-results").innerHTML = `<div class="module" id="stockInfo">
     <h2 style="text-decoration: underline;">Stock Info</h2>
     <p id="price">Price</p>
     <button onclick="userSelectsStock()" class="btn interactStockButton">Add to portfolio</button>
@@ -344,7 +386,7 @@ function closeWebSocket() {
 
 function initWebSocket(object, id) {
     let symbol = object["symbol"];
-    
+
 
     let socket = new WebSocket("wss://ws.finnhub.io?token=c548e3iad3ifdcrdgh80");
     socket.onmessage = function (e) {
@@ -410,21 +452,21 @@ function displayHistory(object, id, replace = false) {
     var lowerBound = new Date();
     lowerBound.setDate(lowerBound.getDate() - 7);
     var newObject = [];
-    
+
     for (key in object) {
         let d = new Date(key.replace(" ", "T"));
         if (d < lowerBound) { break; }
         let p = document.createElement("p");
         let openPrice = (object[key]["1. open"]);
-        
+
         var dateObject = {};
-        
+
         dateObject.date = d;
         dateObject.value = openPrice;
-        
+
         newObject.push(dateObject);
     }
-    
+
     loadChart(newObject, id, replace);
 }
 
@@ -436,25 +478,25 @@ function displayValue(infoObject, priceObject, id, shareCount) {
     console.log(newsLoaded)
     priceLoaded = currentPrice
     document.getElementById("searchButton").innerHTML = "Search";
-    
+
     let title = document.createElement("h2");
     title.innerHTML = newsLoaded;
     title.className = "stockViewTitle";
-    
+
     let price = document.createElement("p");
     price.innerHTML = "Current: " + currentPrice + " (<span class=\"" + ((currentDiff < 0) ? "negativeStock\">" : "positiveStock\">+") + currentDiff + "</span>) at " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     price.className = "stockViewPrice";
-    
+
     let highLow = document.createElement("p");
     highLow.innerHTML = "High/Low: " + priceObject["h"] + " / " + priceObject["l"];
     highLow.className = "stockViewHighLow";
-    
+
     document.getElementById(id).append(title);
     document.getElementById(id).append(price);
     document.getElementById(id).append(highLow);
     renderSellButton();
-    
-    
+
+
 }
 
 //search keylistener
@@ -473,15 +515,15 @@ function onSearchBlurred() {
     document.removeEventListener("keydown", searchOnEnter);
 }
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", function(e) {
-       updateChartColors();
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", function (e) {
+    updateChartColors();
 });
 
 function initAllStockViews() {
     var stocksObj = {}
     var countFunc = keys => {
         stocksObj[keys] = ++stocksObj[keys] || 1;
-    }   
+    }
     userstocks.forEach(countFunc);
     document.getElementById("allStocksHolder").innerHTML = "";
     document.getElementById("stock-port-data").innerHTML = "";
@@ -492,15 +534,15 @@ function initAllStockViews() {
         let div = document.createElement("div");
         div.className = "stockViewContainer module";
         div.id = key;
-        
+
         let title = document.createElement("h1");
-        title.innerHTML = key + "(Shares: " + value +")";
-        
+        title.innerHTML = key + "(Shares: " + value + ")";
+
         div.appendChild(title);
         document.getElementById("allStocksHolder").appendChild(div);
-        
-        getPriceForObject({"symbol":key, "description":usernews[i]}, key, value);
-        getHistoryForObject({"symbol":key}, key);
+
+        getPriceForObject({ "symbol": key, "description": usernews[i] }, key, value);
+        getHistoryForObject({ "symbol": key }, key);
         i++;
     }
 }
