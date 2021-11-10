eadd8d2041068fea16016388365feab13773c9ae
Justin Hamilton <jwhamilton99@gmail.com>
2021-11-10 20:51:36 +0000

    stock container, navbar, user wallet

--- scripts/user.js
+++ scripts/user.js
@@ -1,25 +1,32 @@
 var username;
 var userwallet;
 
+
+
 function resetStocksAndNews() {
 	sessionStorage.removeItem("news");
 	sessionStorage.removeItem("stocks");
+	window.location.reload();
 }
 
 function updateUsername(newUsername) {
 	sessionStorage.setItem("name", newUsername);
+	window.location.reload();
 }
 
 function updatePassword(newPassword) {
 	alert("not yet implemented");
 }
 
-function updatePFP(newPFP) {
+function updatePFP() {
 	alert("not yet implemented");
 }
 
-function updateWallet(newAmount) {
+function updateWallet(newAmount, reload = false) {
 	sessionStorage.setItem("wallet", (Math.round(newAmount*100))/100);
+	if(reload) {
+		window.location.reload();
+	}
 }
 
 function updateStocks(newStocks) {
@@ -36,10 +43,10 @@ function getUserInfo() {
 	return {
 		"username": sessionStorage.getItem("name"),
 		"password": "not implemented yet",
-		"wallet": parseFloat(sessionStorage.getItem("wallet")),
+		"wallet": parseFloat((sessionStorage.getItem("wallet") == null) ? 0 : sessionStorage.getItem("wallet")),
 		"stocks": JSON.parse(sessionStorage.getItem("stocks")),
 		"news": JSON.parse(sessionStorage.getItem("news")),
-		"pfp": "not implemented yet"
+		"pfp": "/img/pfpPlaceholder.svg"
 	};
 }
 
@@ -49,7 +56,26 @@ function displayUserInfo() {
 	link.href="user.html";
 	link.className="nav-link";
 	link.style.padding = "10px";
-	link.innerHTML = userInfo["username"]+"<br />$"+userInfo["wallet"];
+	var pfp = userInfo["pfp"];
+	var wallet = userInfo["wallet"];
+	
+	var userInfoContainer = document.createElement("div");
+	userInfoContainer.id = "userInfoContainerNavbar";
+	
+	let pfpPreview = document.createElement("img");
+	pfpPreview.id = "userPFPNavbar";
+	pfpPreview.src = pfp;
+	pfpPreview.style.height = "10%";
+	
+	let walletAmountPreview = document.createElement("p");
+	walletAmountPreview.id = "walletAmountPreviewNavbar";
+	walletAmountPreview.innerHTML = "$"+wallet;
+	
+	userInfoContainer.appendChild(pfpPreview);
+	userInfoContainer.appendChild(walletAmountPreview);
+	
+	link.appendChild(userInfoContainer);
+	
 	document.getElementById("nav-user").innerHTML = "";
 	document.getElementById("nav-user").appendChild(link);
 }
