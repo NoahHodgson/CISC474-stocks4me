c09a0921f46a4cb28637e43d7fb8d5dc43a1ffb8
Justin Hamilton <jwhamilton99@gmail.com>
2021-11-11 19:15:46 +0000

    get https working

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var app = express();
 
@@ -8,23 +7,17 @@ app.use(express.static('public'));
 app.listen(8080);
 
 const stocks = require('./scripts/stocks.js');
-const fetch = require('cross-fetch');
+const search = require('./scripts/search.js');
+const https = require('https');
 
 app.post("/getUserData", function(req, res) {
 	
 });
 
-app.get("/getStockInfo", function(req, res) {
+app.post("/searchForStock", function(req, res) {
 	(async () => {
-		try {
-			const response = await stocks.loadStock({
-				"symbol":"AAPL"
-			});
-			res.send(response);
-		} catch (error) {
-			console.log(error);
-			res.send("nope");
-		}
+		let response = await search.searchForSymbol("AAPL");
+		res.send(response);
 	})();
 });
 
